fa6361bb9c94ef4c415725dcc27869df79f62b5b
FabianHeng <fabianheng@gmail.com>
2025-03-08 11:57:49 +0800

    Add support for remark command

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,4 +1,16 @@
 package seedu.address.logic.commands;
 
-public class RemarkCommand {
+import seedu.address.model.Model;
+
+/**
+ * Changes the remark of an existing person in the address book.
+ */
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from remark");
+    }
 }
